e6a4d008f6869504818cfcd14afb0523216ce3f5
Gennady Myasnikov <job.myasnikov.gennady@gmail.com>
2018-01-16 16:00:58 +0300

    security interfaces and interface implementation added

---
+++ src/main/java/com/example/demo/service/security/SecurityUserDetailsService.java
@@ -0,0 +1,6 @@
+package com.example.demo.service.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface SecurityUserDetailsService extends UserDetailsService { }
---
+++ src/main/java/com/example/demo/service/security/impl/SecurityUserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.demo.service.security.impl;
+
+import com.example.demo.data.UserRepository;
+import com.example.demo.domain.User;
+import com.example.demo.service.security.SecurityUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SecurityUserDetailsServiceImpl implements SecurityUserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+
+        if(user == null)
+            throw new UsernameNotFoundException(String.format("The username %s doesn't exist", username));
+
+        List<GrantedAuthority> autorities = new ArrayList<>();
+        autorities.add(new SimpleGrantedAuthority(user.getRole()));
+
+        return  new org.springframework.security.core.userdetails.
+                User(user.getUsername(), user.getPassword(), autorities);
+    }
+}

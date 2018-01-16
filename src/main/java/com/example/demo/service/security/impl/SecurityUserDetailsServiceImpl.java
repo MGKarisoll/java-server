10a38ddde77470e9fe57d6969a59106e677eba03
Gennady Myasnikov <job.myasnikov.gennady@gmail.com>
2018-01-16 16:31:11 +0300

    SecurityConfig and AuthorizationServerConfig added

--- src/main/java/com/example/demo/service/security/impl/SecurityUserDetailsServiceImpl.java
+++ src/main/java/com/example/demo/service/security/impl/SecurityUserDetailsServiceImpl.java
@@ -8,10 +8,12 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class SecurityUserDetailsServiceImpl implements SecurityUserDetailsService {
     @Autowired
     private UserRepository userRepository;
